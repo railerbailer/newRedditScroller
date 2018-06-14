28f9482e713dad08f7d357984a3be01dd6df7333
railerbailer <rasmus.arvidsson1@gmail.com>
2018-06-14 14:54:08 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component,Fragment } from 'react'
 import 'antd/dist/antd.css';
-import { Icon } from 'antd';
+import { Icon, Button } from 'antd';
 import {subredditArray, secondTheme} from './subreddits'
 import logo from './logo.svg'
 import './App.css'
@@ -10,7 +10,12 @@ import Categories from './components/startpage'
 
 
 // import ArrowKeysReact from 'arrow-keys-react';
+const styles={
+  appWrap:{
+     
 
+  }
+}
 
 class App extends Component {
   constructor(props){
@@ -21,10 +26,54 @@ class App extends Component {
 
 
     this.state={
+      nsfwAccept: false,
       category: 'nothing',
       autoplay: true,
     }  
   }
+
+  switchOnThem(){
+    switch(this.state.nsfwAccept){
+      case false:
+      console.log('TWOTRUE')
+      break;
+      case true:
+      console.log('ONETRUE')
+
+    }
+  }
+
+  renderContent(){
+    if(this.state.category!=='nothing'&&this.state.category==='NORMAL'){
+      return(<Scroller autoplay={this.state.autoplay} categorySet={this.categorySet} category={this.state.category}/>
+      )
+    }
+
+    if(this.state.category!=='nothing'){
+      if(this.state.nsfwAccept===false){
+            return (
+              <div style={{position: 'absolute',top: '25%', left: '40%', align: 'center', textAlign: 'center'}}>
+                    <p style={{color: 'white'}}>NSFW CONTENT! Are you over 18</p>
+                    <Button onClick={()=>this.setState({nsfwAccept: true})}>Yes</Button>
+                    <Button  onClick={()=>this.setState({nsfwAccept: false, category: 'nothing'})}>No</Button>             
+              </div>
+
+              )
+        }
+
+        else return(<Scroller autoplay={this.state.autoplay} categorySet={this.categorySet} category={this.state.category}/>)
+
+      
+    }
+    
+    else{
+      return(
+        <Categories autoplayPress={this.autoplayPress} autoplay={this.state.autoplay} categorySet={this.categorySet}/>
+        )
+    }
+
+  }
+
   categorySet(val){
    this.setState({category: val})
   }
@@ -32,19 +81,16 @@ class App extends Component {
     this.setState({autoplay: !this.state.autoplay})
   }
   render() {
-    console.log(this.state.autoplay)
+    
 
     return (
-      <div>
+      <div style={styles.appWrap}>
         
-
         {
-          this.state.category!=='nothing'
-          ?<Scroller autoplay={this.state.autoplay} categorySet={this.categorySet} category={this.state.category}/>
-          :<Categories autoplayPress={this.autoplayPress} autoplay={this.state.autoplay} categorySet={this.categorySet}/>
-
-
+          this.renderContent() 
         }
+
+
       </div>
     );
   }
